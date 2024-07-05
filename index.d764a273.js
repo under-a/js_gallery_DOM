b5e8073a23670cec5e217b70c8c585793fec811c
under-a <mymail15@i.ua>
2024-07-05 12:38:09 +0300

    make build

---
+++ index.d764a273.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery"),t=document.querySelector(".gallery__large-img");e.addEventListener("click",function(e){e.preventDefault(),t.src=e.target.closest(".list-item__link").href});
+//# sourceMappingURL=index.d764a273.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const gallery = document.querySelector(".gallery");
+const largeImg = document.querySelector(".gallery__large-img");
+gallery.addEventListener("click", (e)=>{
+    e.preventDefault();
+    largeImg.src = e.target.closest(".list-item__link").href;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
